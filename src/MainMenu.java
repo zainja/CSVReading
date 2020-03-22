f2481a664221c2963b735436beb51fd0b9c01ed9
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-03-22 15:32:24 +0000

    Initial commit

---
+++ src/MainMenu.java
@@ -0,0 +1,50 @@
+//package com.mkyong.csv;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class MainMenu {
+
+    public static void main(String[] args) {
+
+        String csvFile = "src/Navigation.csv";
+        String csvSplitBy = ";";
+        ArrayList<MenuObject> menuObjects = new ArrayList<>();
+
+        // read files from all opsys
+        // String fileLocation = File.separator + "D:" + File.separator + "Navigation.csv";
+
+        try {
+            File iFile = new File(csvFile);
+            // File iFile = new File("D://Navigation.csv");
+            //System.out.println(iFile);
+            Scanner iScan = new Scanner(iFile);
+            int line = 0;
+            while (iScan.hasNext()) {
+                String nextLine = iScan.nextLine();
+                if(line > 0)
+                {
+                    String[] arr = nextLine.split(csvSplitBy);
+                    int id = Integer.parseInt(arr[0]);
+                    String menuName = arr[1];
+                    int parentID = (arr[2].equals("NULL"))? 0:Integer.parseInt(arr[2]);
+                    boolean isHidden = arr[3].equals("False");
+                    String linkURL = arr[4];
+                    MenuObject menuObject = new MenuObject(id, menuName, parentID, isHidden, linkURL);
+                    menuObjects.add(menuObject);
+                }
+                line ++;
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Collections.sort(menuObjects);
+        for (MenuObject menu:
+             menuObjects) {
+            System.out.println(menu.toString());
+        }
+
+    }
+}
---
+++ src/MenuObject.java
@@ -0,0 +1,57 @@
+public class MenuObject implements Comparable<MenuObject>{
+    private int ID;
+    private String MenuName;
+    private int ParentID;
+    private boolean isHidden;
+    private String LinkURL;
+    public static int classAncestor = 0;
+    int currentAncestor;
+    public int getId() {
+        return ID;
+    }
+
+    public MenuObject(int ID, String menuName, int parentID, boolean isHidden, String linkURL) {
+        this.ID = ID;
+        MenuName = menuName;
+        ParentID = parentID;
+        this.isHidden = isHidden;
+        LinkURL = linkURL;
+        currentAncestor = classAncestor;
+        classAncestor ++;
+    }
+
+    public void setID(int newID) {
+        this.ID = newID;
+    }
+
+    public int getParentID() {
+        return ParentID;
+    }
+
+    public void setMenuName(String newMenuName) {
+        this.MenuName = newMenuName;
+    }
+    void setParentID(int newParentID){
+        this.ParentID = newParentID;
+    }
+    public void setisHidden(boolean newisHidden) {
+        this.isHidden = newisHidden;
+    }
+    public void setLinkURL(String newLinkURL) {
+        this.LinkURL = newLinkURL;
+    }
+    public int getCurrentAncestor()
+    {
+        return this.currentAncestor;
+    }
+
+    @Override
+    public String toString() {
+        return this.getId() + " " + this.getParentID() + this.MenuName;
+    }
+
+    @Override
+    public int compareTo(MenuObject object) {
+        return Integer.compare(this.getParentID(), object.getParentID());
+    }
+}
